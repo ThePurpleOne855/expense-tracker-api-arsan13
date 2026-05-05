d70eafb2e6736cb86f1c1b7c25422f6369578845
Jose Santana <jsantana@2603-9001-3000-07a5-0000-0000-0000-1ccc.inf6.spectrum.com>
2026-05-04 22:31:57 -0400

    Dependency changes, Deletes, refactored entities v 0.2

--- src/main/java/com/arsan/expense/entity/Transaction.java
+++ src/main/java/com/arsan/expense/entity/Transaction.java
@@ -8,9 +8,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.Builder;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transaction {
 
 	@Id
@@ -30,79 +39,5 @@ public class Transaction {
 	@JoinColumn()
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Category category;
-
-	public Transaction() {
-	}
-
-	public Transaction(Integer id, Double amount, String note, Long transactionDate, boolean remind) {
-		this.id = id;
-		this.amount = amount;
-		this.note = note;
-		this.transactionDate = transactionDate;
-		this.remind = remind;
-	}
 	
-	public Transaction(Double amount, String note, Long transactionDate, boolean remind) {
-		this.amount = amount;
-		this.note = note;
-		this.transactionDate = transactionDate;
-		this.remind = remind;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public Double getAmount() {
-		return amount;
-	}
-
-	public void setAmount(Double amount) {
-		this.amount = amount;
-	}
-
-	public String getNote() {
-		return note;
-	}
-
-	public void setNote(String note) {
-		this.note = note;
-	}
-
-	public Long getTransactionDate() {
-		return transactionDate;
-	}
-
-	public void setTransactionDate(Long transactionDate) {
-		this.transactionDate = transactionDate;
-	}
-	
-	public boolean isRemind() {
-		return remind;
-	}
-
-	public void setRemind(boolean remind) {
-		this.remind = remind;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-	
-	public Category getCategory() {
-		return category;
-	}
-
-	public void setCategory(Category category) {
-		this.category = category;
-	}
-
 }
--- src/main/java/com/arsan/expense/entity/User.java
+++ src/main/java/com/arsan/expense/entity/User.java
@@ -11,8 +11,17 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 @Entity
 @Table(name="person")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
 	@Id
@@ -29,67 +38,6 @@ public class User {
 	@OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
 	private List<Transaction> transactions;
 	
-	public User() {
-	}
-
-	public User(Integer id, String firstName, String lastName, String email, String password) {
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.password = password;
-	}
 	
-	public User(String firstName, String lastName, String email, String password) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.password = password;
-	}
-
-	public User(String email, String password) {
-		this.email = email;
-		this.password = password;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 	
 }
